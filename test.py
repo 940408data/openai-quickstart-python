fd16d613186abde72e0feedb1520b88751f9650f
huangxu3 <huangxu3@newhope.cn>
2023-03-13 23:33:55 +0800

    update

---
+++ test.py
@@ -0,0 +1,26 @@
+import openai
+openai.api_key = "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" # 替换成您的API密钥
+prompt = "Hello, how are you?" # 您的对话开始语
+
+prompt1 = """
+Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: white horse
+Names: """
+
+prompt2 ="""
+《Oxford English Dictionary》中对水的定义，展示原文及其中文翻译
+"""
+
+prompt2 ="""
+"""
+
+model = "text-davinci-002" # 模型ID
+response = openai.Completion.create(engine=model, prompt=prompt1, max_tokens=2000)
+text = response['choices'][0]['message']['content']
+print(text)
+print(response['usage'])
