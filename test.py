a678222f219d22e3521ebdd89cb9e6ed70c64f68
940408data <1475559715@qq.com>
2023-03-15 10:53:59 +0800

    update

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import openai
 import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 prompt = "Hello, how are you?" # 您的对话开始语
@@ -18,11 +22,11 @@ prompt2 ="""
 《Oxford English Dictionary》中对水的定义，展示原文及其中文翻译
 """
 
-prompt2 ="""
+prompt3 ="""
 """
 
 model = "text-davinci-002" # 模型ID
 response = openai.Completion.create(engine=model, prompt=prompt1, max_tokens=2000)
-text = response['choices'][0]['message']['content']
+text = response['choices'][0].text
 print(text)
-print(response['usage'])
+#print(response['usage'])
